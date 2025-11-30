32e14020adf65dddad609b99d099d3cee9817b8b
UPMP HK <upmphk@gmail.com>
2025-11-30 18:13:04 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  // bisa dibiarkan kosong
+});
+
+self.addEventListener("activate", (event) => {
+  // bisa dibiarkan kosong
+});
+
+self.addEventListener("fetch", (event) => {
+  // biarkan lewat saja
+});
